7b1830bd1770eb193216c08af5c6606bb396513f
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-02 23:50:18 +0530

    minor changes in UC2

--- src/com/yml/adressbooksystem/AddressBookMain.java
+++ src/com/yml/adressbooksystem/AddressBookMain.java
@@ -12,10 +12,11 @@ public class AddressBookMain {
 		Scanner in = new Scanner(System.in);
 		Contact contact = new Contact();
 		while(true) {
-			System.out.println("Enter your choice\n 1.Add a new contact \n2.Display address book\n3.Exit\n");
+			System.out.println("\nEnter your choice\n1.Add a new contact \n2.Display address book\n3.Exit\n");
 			int choice = in.nextInt();
 			switch(choice) {
 			case 1:
+				in.nextLine();
 				System.out.println("Enter the first name");
 				String firstName = in.nextLine();
 				contact.setFirstName(firstName);
@@ -31,18 +32,21 @@ public class AddressBookMain {
 				System.out.println("Enter the state");
 				String state = in.nextLine();
 				contact.setState(state);
+				System.out.println("Enter the email");
+				String email = in.nextLine();
+				contact.setEmail(email);
 				System.out.println("Enter the zip code");
 				int zip = in.nextInt();
 				contact.setZip(zip);
 				System.out.println("Enter the phone");
 				BigInteger phone = in.nextBigInteger();
 				contact.setPhone(phone);
-				System.out.println("Enter the email");
-				String email = in.nextLine();
-				contact.setEmail(email);
 				addressBook.addContact(contact);
+				break;
 			case 2:
+				System.out.println("\nContacts:\n");
 				addressBook.displayContact();
+				break;
 			case 3:
 				return;
 		}
@@ -50,7 +54,6 @@ public class AddressBookMain {
 			
 			
 		}
-		
 	}
 
 }
