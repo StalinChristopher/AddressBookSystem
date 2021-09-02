42381a125b49a1f51e8b009c2f928b9ce435fdb6
StalinChristopher <stalin.dsouza@ymedialabs.com>
2021-09-02 22:47:00 +0530

    Initialized project

---
+++ src/com/yml/adressbooksystem/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.yml.adressbooksystem;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+
+	}
+
+}
